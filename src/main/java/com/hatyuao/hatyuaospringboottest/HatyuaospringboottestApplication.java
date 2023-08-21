4020adb9278565896ac92a2d700f3f40404e7347
hatyuao <hayuao@126.com>
2023-08-21 20:07:40 +0800

    初始文件提交测试

---
+++ src/main/java/com/hatyuao/hatyuaospringboottest/HatyuaospringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.hatyuao.hatyuaospringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatyuaospringboottestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HatyuaospringboottestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hatyuao/hatyuaospringboottest/HatyuaospringboottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatyuao.hatyuaospringboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatyuaospringboottestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
