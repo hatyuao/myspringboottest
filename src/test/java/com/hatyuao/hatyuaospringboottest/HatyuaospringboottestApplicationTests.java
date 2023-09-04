fb05b58bb8ea3ffc487a96cff7c9fe9a1522005d
hatyuao <hayuao@126.com>
2023-09-04 16:39:20 +0800

    测试

--- src/test/java/com/hatyuao/hatyuaospringboottest/HatyuaospringboottestApplicationTests.java
+++ src/test/java/com/hatyuao/hatyuaospringboottest/HatyuaospringboottestApplicationTests.java
@@ -2,11 +2,13 @@ package com.hatyuao.hatyuaospringboottest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class HatyuaospringboottestApplicationTests {
 
     @Test
+    @Transactional()
     void contextLoads() {
     }
 
